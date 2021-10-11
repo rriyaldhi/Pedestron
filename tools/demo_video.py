df6cebcdff7676ef4e3f4efdfc05b07ec69bb562
rriyaldhi <rriyaldhi@gmail.com>
2021-10-11 16:19:33 +0200

    add demo_video

---
+++ tools/demo_video.py
@@ -0,0 +1,107 @@
+import argparse
+
+import os
+import os.path as osp
+import sys
+sys.path.insert(0, osp.join(osp.dirname(osp.abspath(__file__)), '../'))
+import time
+import cv2
+import torch
+import glob
+import json
+import mmcv
+import numpy as np
+import pycocotools.mask as maskUtils
+
+from mmdet.apis import inference_detector, init_detector
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='MMDet test detector')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    parser.add_argument('output_dir', type=str, help='the dir for result images')
+    parser.add_argument(
+        '--launcher',
+        choices=['none', 'pytorch', 'slurm', 'mpi'],
+        default='none',
+        help='job launcher')
+    parser.add_argument('--local_rank', type=int, default=0)
+    parser.add_argument('--mean_teacher', action='store_true', help='test the mean teacher pth')
+    args = parser.parse_args()
+    return args
+
+def create_base_dir(dest):
+    basedir = os.path.dirname(dest)
+    if not os.path.exists(basedir):
+        os.makedirs(basedir)
+
+def run_detector_on_dataset(video):
+    args = parse_args()
+    # input_dir = args.input_img_dir
+    output_dir = args.output_dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    model = init_detector(
+        args.config, args.checkpoint, device=torch.device('cuda:0'))
+
+    cap = cv2.VideoCapture('videos/' + video)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    out = cv2.VideoWriter('videos/output/' + video, cv2.VideoWriter_fourcc('m','p', '4', 'v'), fps, (width,height))
+    while(cap.isOpened()):
+        ok, frame = cap.read()
+        if ok:
+            results = inference_detector(model, frame)
+            result_frame = show_result(frame, results, model.CLASSES, out_file=None)
+            out.write(result_frame)
+        else:
+            break
+    out.release()
+    cap.release()
+
+def show_result(img, result, class_names, score_thr=0.3, out_file=None):
+    """Visualize the detection results on the image.
+
+    Args:
+        img (str or np.ndarray): Image filename or loaded image.
+        result (tuple[list] or list): The detection result, can be either
+            (bbox, segm) or just bbox.
+        class_names (list[str] or tuple[str]): A list of class names.
+        score_thr (float): The threshold to visualize the bboxes and masks.
+        out_file (str, optional): If specified, the visualization result will
+            be written to the out file instead of shown in a window.
+    """
+    assert isinstance(class_names, (tuple, list))
+    img = mmcv.imread(img)
+    if isinstance(result, tuple):
+        bbox_result, segm_result = result
+    else:
+        bbox_result, segm_result = result, None
+    bboxes = np.vstack(bbox_result)
+    # draw segmentation masks
+    if segm_result is not None:
+        segms = mmcv.concat_list(segm_result)
+        inds = np.where(bboxes[:, -1] > score_thr)[0]
+        for i in inds:
+            color_mask = np.random.randint(0, 256, (1, 3), dtype=np.uint8)
+            mask = maskUtils.decode(segms[i]).astype(np.bool)
+            img[mask] = img[mask] * 0.5 + color_mask * 0.5
+    # draw bounding boxes
+    labels = [
+        np.full(bbox.shape[0], i, dtype=np.int32)
+        for i, bbox in enumerate(bbox_result)
+    ]
+    labels = np.concatenate(labels)
+    return mmcv.imshow_det_bboxes(
+        img.copy(),
+        bboxes,
+        labels,
+        class_names=class_names,
+        score_thr=score_thr,
+        show=False,
+        out_file=out_file)
+
+if __name__ == '__main__':
+    run_detector_on_dataset('n.mp4')
